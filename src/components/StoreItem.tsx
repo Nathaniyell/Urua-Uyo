b3b2f970d4d283e013ea731840e0847b8c45a610
Nathaniyell <essien.nathan@yahoo.com>
2023-12-19 15:29:06 +0100

    update StoreItem.tsx

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -11,9 +11,16 @@ export const StoreItem = ({id, title, price, image}:StoreItemProps) => {
   return (
         <Card key={id}>
             <Card.Img variant="top" src={image}
-            height="200px" 
-            style={{objectFit: "cover"}}
+            height="300px" 
+            style={{objectFit: "contain"}}
             />
+            <Card.Body className="d-flex flex-column">
+<Card.Title className="d-flex justify-content-between align-items-baseline my-4">
+    <span className="fs-5">{title}</span>
+    <span className="ms-2 text-muted">${price}</span>
+</Card.Title>
+
+            </Card.Body>
         </Card>
   )
 }
