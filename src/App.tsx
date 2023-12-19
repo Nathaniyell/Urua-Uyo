9afb1cd1049f270f1e7473ad7429127b938d3d39
Nathaniyell <essien.nathan@yahoo.com>
2023-12-19 13:07:10 +0100

    update index.html and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Store from "./pages/Store"
-import { Container } from "react-bootstrap"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Store from "./pages/Store";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
-
-    <Routes>
+    <Container>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/store" element={<Store />} />
-    </Routes>
-    // <Container>HI</Container>
-  )
-}
+      </Routes>
+    </Container>
+  );
+};
 
-export default App
+export default App;
