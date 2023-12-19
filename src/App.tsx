97fb70cdc8b097f30e67f024bde0fba7b6f1491b
Nathaniyell <essien.nathan@yahoo.com>
2023-12-19 13:03:11 +0100

    Implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import { Container } from "react-bootstrap"
 
 const App = () => {
   return (
-    <div>HI</div>
+
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/store" element={<Store />} />
+    </Routes>
+    // <Container>HI</Container>
   )
 }
 
