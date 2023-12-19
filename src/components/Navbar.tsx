074a049f33380e70b07d69184ded4bbf38319c79
Nathaniyell <essien.nathan@yahoo.com>
2023-12-19 13:28:02 +0100

    build(deps): update package-lock.json, package.json and Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import {BsCart2} from "react-icons/bs"
 const Navbar = () => {
   return (
-    <NavbarBs>
+    <NavbarBs sticky="top">
       <Container className="bg-white shadow-sm mb-3">
         
         <Nav className="me-auto">
@@ -17,7 +18,7 @@ const Navbar = () => {
             Store
           </Nav.Link>
         </Nav>
-        <Button></Button>
+        <Button style={{width: "3rem", height: "3rem", position: "relative"}} variant="outline-primary" className="rounded-circle"><BsCart2 /> <div className="rounded-circle d-flex justify-content-center align-items-center" style={{color: "#fff", width: "1.5rem", height: "1.5rem", right:0, top: 0, transform:"translate(25% 25%)"}}>0</div></Button>
       </Container>
     </NavbarBs>
   );
