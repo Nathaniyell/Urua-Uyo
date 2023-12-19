a96037635d3ac01479cddf8436ee2883c347f35d
Nathaniyell <essien.nathan@yahoo.com>
2023-12-19 18:49:00 +0100

    update Store.tsx

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -3,22 +3,23 @@ import { Col, Row } from "react-bootstrap";
 import { useProductContext } from "../data/data";
 import { StoreItem } from "../components/StoreItem";
 
-
-
 const Store = () => {
-
-const { products } = useProductContext()
+  const { products } = useProductContext();
   return (
     <>
       <h1>Store</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
-        {products?.map((product) => {
-          return (
-            <Col key={product.id}>
-              <StoreItem {...product} />
-            </Col>
-          );
-        })}
+        {products.length === 0 ? (
+          <h1>Loading...</h1>
+        ) : (
+          products?.map((product) => {
+            return (
+              <Col key={product.id}>
+                <StoreItem {...product} />
+              </Col>
+            );
+          })
+        )}
       </Row>
     </>
   );
