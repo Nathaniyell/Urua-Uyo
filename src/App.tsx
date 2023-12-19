ae50f50434d12fb47056aacd99780cc79e45c64e
Nathaniyell <essien.nathan@yahoo.com>
2023-12-19 13:04:01 +0100

    update App.tsx and About.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Store from "./pages/Store"
 import { Container } from "react-bootstrap"
 
 const App = () => {
