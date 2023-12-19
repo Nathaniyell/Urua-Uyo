911559ddd4f832ae250ef91f121457e7e6db6c2a
Nathaniyell <essien.nathan@yahoo.com>
2023-12-19 16:52:00 +0100

    update ShoppingCartContext.tsx

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useContext, useState } from 'react';
+import { ReactNode, createContext, useContext, useState } from "react";
 
 // Define the type for your shopping cart context
 type ShoppingCartProviderProps = {
@@ -6,79 +6,82 @@ type ShoppingCartProviderProps = {
 };
 type ShoppingCartContextType = {
   // Define the properties and methods you want in the context
-getItemQuantity:(id:number)=> number
-increaseCartQuantity:(id:number)=> void
-decreaseCartQuantity:(id:number)=> void
-removeFromCart:(id:number)=> void
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
 };
-type CartItem ={
-  id: number
-  quantity: number
-}
+type CartItem = {
+  id: number;
+  quantity: number;
+};
 
-const ShoppingCartContext = createContext<ShoppingCartContextType | undefined>(undefined);
+const ShoppingCartContext = createContext<ShoppingCartContextType | undefined>(
+  undefined
+);
 
 export const useShoppingCart = () => {
   const context = useContext(ShoppingCartContext);
   if (!context) {
-    throw new Error('useShoppingCart must be used within a ShoppingCartProvider');
+    throw new Error(
+      "useShoppingCart must be used within a ShoppingCartProvider"
+    );
   }
   return context;
 };
 
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
-export function ShoppingCartProvider({ children }: ShoppingCartProviderProps){
-  const [cartItems, setCartItems] = useState<CartItem[]>([])
-
-  function getItemQuantity(id:number){
-    cartItems.find(item=>item.id===id)?.quantity||0 
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
   }
-  function increaseCartQuantity(id:number){
-   setCartItems(currItems=>{
-    if(currItems.find(item=>item.id===id) == null){
-      return[...currItems, {id, quantity:1}]
-    }else{
-      return currItems.map(item=>{
-        if(item.id===id){
-          return{...item, quantity: item.quantity +1}
-        }else{
-          return item
-        }
-
-      })
-    }
-   })
+  function increaseCartQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id) == null) {
+        return [...currItems, { id, quantity: 1 }];
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
   }
-  function decreaseCartQuantity(id:number){
-   setCartItems(currItems=>{
-    if(currItems.find(item=>item.id===id)?.quantity ===1){
-      return currItems.filter(item=>item.id!==id)
-    }else{
-      return currItems.map(item=>{
-        if(item.id===id){
-          return{...item, quantity: item.quantity -1}
-        }else{
-          return item
-        }
-
-      })
-    }
-   })
+  function decreaseCartQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id)?.quantity === 1) {
+        return currItems.filter((item) => item.id !== id);
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
   }
 
-  const removeFromCart = (id:number)=>{
-setCartItems(currItems=>{
-  return currItems.filter(item=>item.id!==id)
-})
-  }
+  const removeFromCart = (id: number) => {
+    setCartItems((currItems) => {
+      return currItems.filter((item) => item.id !== id);
+    });
+  };
 
   return (
-    <ShoppingCartContext.Provider 
-    value={{ 
-      getItemQuantity,
-      increaseCartQuantity, 
-      decreaseCartQuantity, 
-      removeFromCart}}>
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+      }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
