8ba98b39c5da2e79a74e62cedf0c0e7bc5ed3713
Nathaniyell <essien.nathan@yahoo.com>
2023-12-19 18:28:15 +0100

    update CartItem.tsx

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,5 +1,7 @@
+import { Button, Stack } from "react-bootstrap"
 import { useShoppingCart } from "../context/ShoppingCartContext"
 import { useProductContext } from "../data/data"
+import { formatCurrency } from "../utilities/formatCurrency"
 
 
 type CartItemProps={
@@ -10,9 +12,21 @@ quantity:number
 const CartItem = ({id, quantity}:CartItemProps ) => {
 const {removeFromCart, increaseCartQuantity, decreaseCartQuantity} = useShoppingCart()
 const { products } = useProductContext()
+const item = products.find(product=>product.id === id)
+if(item == null)return null
 
   return (
-    <div>CartItem</div>
+  <Stack direction="horizontal" className="flex align-items-center">
+    <img src={item.image} style={{width: "150px", height: "80px", objectFit: "contain"}} />
+    <div className="me-auto">
+        <div>
+            {item.title} {quantity>1 && <span className="muted" style={{fontSize: ".65rem"}}>x{quantity}</span>}
+        </div>
+        <div className="muted" style={{fontSize: ".75rem"}}>{formatCurrency(item.price)}</div>
+    </div>
+    <div>{formatCurrency(item.price * quantity)}</div>
+    <Button variant="danger" onClick={()=>removeFromCart(item.id)} size="sm">x</Button>
+  </Stack>
   )
 }
 
