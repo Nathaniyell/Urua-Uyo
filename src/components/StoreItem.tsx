79a63e9541e540a65393f0306c9537aa92a3cb89
Nathaniyell <essien.nathan@yahoo.com>
2023-12-19 14:41:45 +0100

    update Store.tsx

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,15 @@
+
+
+
+type StoreItemProps = {
+    id: number,
+    title: string,
+    price: number,
+    imgUrl: string
+}
+
+export const StoreItem = ({id, title, price, imgUrl}:StoreItemProps) => {
+  return (
+    <div>StoreItem</div>
+  )
+}
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,16 +1,19 @@
 import { Col, Row } from "react-bootstrap";
 import products from "../data/data";
+import { StoreItem } from "../components/StoreItem";
 const Store = () => {
   return (
   <>
   <h1>Store</h1>
-    <Row >
+    <Row md={2} xs={1} lg={3} className="g-3" >
       {
         products.map(product=>{
-          const {id, image,title,price} = product
           return(
 
-            <Col></Col> 
+            <Col key={product.id}>
+              <StoreItem {...product} />
+
+            </Col> 
           )
         })
       }
