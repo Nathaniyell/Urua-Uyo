81fd6b5fb8a0339fa73532dfaa1fee727989536b
Nathaniyell <essien.nathan@yahoo.com>
2023-12-20 16:01:35 +0100

    update ShoppingCartContext.tsx

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -13,61 +13,93 @@ type ShoppingCartContextType = {
   increaseCartQuantity: (id: number) => void;
   decreaseCartQuantity: (id: number) => void;
   removeFromCart: (id: number) => void;
-cartQuantity: number
-cartItems:CartItem[]
-openCart:()=>void
-closeCart:()=>void
-cartIsOpen: boolean
-
+  cartQuantity: number;
+  cartItems: CartItem[];
+  openCart: () => void;
+  closeCart: () => void;
+  cartIsOpen: boolean;
 };
-
 
 type CartItem = {
   id: number;
   quantity: number;
 };
 
+// Create a context for the shopping cart with an initial value of undefined
 const ShoppingCartContext = createContext<ShoppingCartContextType | undefined>(
   undefined
 );
 
+// Export a custom hook to use the shopping cart context
 export const useShoppingCart = () => {
+  // Access the shopping cart context
   const context = useContext(ShoppingCartContext);
+  // Throw an error if the hook is not used within a ShoppingCartProvider
   if (!context) {
     throw new Error(
       "useShoppingCart must be used within a ShoppingCartProvider"
     );
   }
+  // Return the context
   return context;
 };
 
-export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
-  const [cartItems, setCartItems] = useLocalSstorage<CartItem[]>("shopping-cart",[])
+// Create a component that provides the shopping cart context to its children
+export function ShoppingCartProvider({
+  children,
+}: ShoppingCartProviderProps) {
+  // Use a custom hook to get and set cart items in local storage
+  const [cartItems, setCartItems] = useLocalSstorage<CartItem[]>(
+    "shopping-cart",
+    []
+  );
+
+  // Use state to track whether the cart is open or closed
   const [cartIsOpen, setCartIsOpen] = useState(false);
 
-const cartQuantity = cartItems.reduce((quantity, item) => quantity + item.quantity, 0);
+  // Calculate the total quantity of items in the cart
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => quantity + item.quantity,
+    0
+  );
 
-  const openCart=()=>setCartIsOpen(true)
-  const closeCart=()=>setCartIsOpen(false)
+  // Function to open the cart
+  const openCart = () => setCartIsOpen(true);
 
-    function getItemQuantity(id: number) {
+  // Function to close the cart
+  const closeCart = () => setCartIsOpen(false);
+
+  // Function to get the quantity of a specific item in the cart
+  //getItemQuantity function takes an id as an argument, searches for an item with that id in the cartItems array, and returns its quantity. If the item is not found, it returns a default value of 0.
+  function getItemQuantity(id: number) {
     return cartItems.find((item) => item.id === id)?.quantity || 0;
   }
+
+  // Function to increase the quantity of a specific item in the cart
   function increaseCartQuantity(id: number) {
+    // Use the setCartItems function to update the cartItems state based on the current items
     setCartItems((currItems) => {
+      // Check if the item with the given id is not present in the current items
       if (currItems.find((item) => item.id === id) == null) {
+        // If not present, create a new array with the current items and add the new item with a quantity of 1
         return [...currItems, { id, quantity: 1 }];
       } else {
+        // If the item is already present, map over the current items array
         return currItems.map((item) => {
+          // If the item id matches the provided id, update the quantity by incrementing it by 1
           if (item.id === id) {
             return { ...item, quantity: item.quantity + 1 };
           } else {
+            // If the item id doesn't match, leave the item unchanged
             return item;
           }
         });
       }
     });
   }
+  
+
+  // Function to decrease the quantity of a specific item in the cart
   function decreaseCartQuantity(id: number) {
     setCartItems((currItems) => {
       if (currItems.find((item) => item.id === id)?.quantity === 1) {
@@ -84,12 +116,14 @@ const cartQuantity = cartItems.reduce((quantity, item) => quantity + item.quanti
     });
   }
 
+  // Function to remove a specific item from the cart
   const removeFromCart = (id: number) => {
     setCartItems((currItems) => {
       return currItems.filter((item) => item.id !== id);
     });
   };
 
+  // Provide the shopping cart context to its children
   return (
     <ShoppingCartContext.Provider
       value={{
@@ -101,7 +135,7 @@ const cartQuantity = cartItems.reduce((quantity, item) => quantity + item.quanti
         cartQuantity,
         openCart,
         closeCart,
-        cartIsOpen
+        cartIsOpen,
       }}
     >
       {children}
