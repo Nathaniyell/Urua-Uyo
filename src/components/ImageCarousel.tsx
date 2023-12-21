87c1bc0651d02e3fd7c74f3861d7b5ae1d1f932c
Nathaniyell <essien.nathan@yahoo.com>
2023-12-22 00:01:44 +0100

    update ImageCarousel.tsx

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -5,22 +5,24 @@ type ImageProps ={
     imageSrc1: string
     imageSrc2: string
     imageSrc3: string
-    imageTitle: string
+    imageTitle1: string
+    imageTitle2: string
+    imageTitle3: string
 }
 
-function ImageCarousel({imageSrc,imageTitle}: ImageProps) {
+function ImageCarousel({imageSrc1,imageSrc2, imageSrc3, imageTitle1, imageTitle2, imageTitle3}: ImageProps) {
   return (
     <Carousel fade>
       <Carousel.Item>
-        <img src={imageSrc} alt={imageTitle} />
+        <img src={imageSrc1} alt={imageTitle1} />
           </Carousel.Item>
       <Carousel.Item>
         
-      <img src={imageSrc} alt={imageTitle} />
+      <img src={imageSrc2} alt={imageTitle2} />
       </Carousel.Item>
       <Carousel.Item>
         
-      <img src={imageSrc} alt={imageTitle} />
+      <img src={imageSrc3} alt={imageTitle3} />
       </Carousel.Item>
     </Carousel>
   );
