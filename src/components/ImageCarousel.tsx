23437f6b1807b69df6a7f4639cda5804cfe8541d
Nathaniyell <essien.nathan@yahoo.com>
2023-12-21 23:54:10 +0100

    add new carousel component for images

---
+++ src/components/ImageCarousel.tsx
@@ -0,0 +1,23 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+
+function CarouselFadeExample() {
+  return (
+    <Carousel fade>
+      <Carousel.Item>
+        <img />
+    
+      </Carousel.Item>
+      <Carousel.Item>
+        
+    
+      </Carousel.Item>
+      <Carousel.Item>
+        
+       
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default CarouselFadeExample;
