13063b73346b7278e46a0b361a944e030e7165c9
Nathaniyell <essien.nathan@yahoo.com>
2023-12-21 23:48:21 +0100

    update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   
   return (
-    <div className="d-flex">
+    <div className="d-flex align-items-center justify-content-between container">
     <div>
       <h1>Discover our latest collection</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus aspernatur, voluptates excepturi quae perspiciatis deserunt! Quod corporis impedit aperiam ad ducimus recusandae assumenda, illo libero tempora quaerat sint quos provident? Asperiores sequi sit quis impedit nemo, beatae itaque nobis assumenda mollitia accusantium debitis tenetur rerum ullam veniam.</p>
