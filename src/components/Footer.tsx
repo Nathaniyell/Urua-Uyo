dbd34cb4386c195b285b5c910dd270d5c769e0ca
Nathaniyell <essien.nathan@yahoo.com>
2023-12-23 17:46:54 +0100

    update Footer.tsx and footer.css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
+
 import "./footer.css"
 
 const Footer = () => {
   return (
-    <footer className="container" style={{backgroundColor: "#552d1ed9"}}>
+    <footer className="p-4 text-white" style={{backgroundColor: "#552d1ed9"}}>
         <h1 className="fs-2 text-white">URUA UYO</h1>
         <form action="">
-            <label htmlFor="">Get Notified of New Producs</label>
+            <label htmlFor="" className="my-2">Get notified of new arrivals</label>
             <div>
                 <input type="email" placeholder="johndoe@email.com" />
                 <button className="footer-btn" type="submit">Submit</button>
