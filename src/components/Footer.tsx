cd481a820ec7151769cd30d7dd77ee7fca9f8529
Nathaniyell <essien.nathan@yahoo.com>
2023-12-25 10:00:33 +0100

    update Footer.tsx and footer.css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,35 +11,35 @@ import visa from "../assets/visa-fill-svgrepo-com.svg"
 
 const Footer = () => {
   return (
-    <footer className="w-100 p-3 p-md-5" style={{backgroundColor: "#552d1ed9"}}>
-    <Row xs={1} md={3} className="text-white mb-5">
- 
+    <footer className="w-100 p-3 p-md-3" style={{backgroundColor: "#552d1ed9"}}>
+    <Row xs={1} md={3} className="text-white justify-content-between mb-5">
+   
         <h1 className="fs-2 text-white mb-3 d-inline w-25">URUA UYO</h1>
-      
-        <Col className="my-3 my-md-0 me-0 me-md-5">
+    
+        <Col>
+        <form action="" className="my-3 my-md-0">
+            <h5 className="text-center mb-3">Get notified of new arrivals</h5>
+            <div>
+                <input className="footer__input mb-2 w-100" type="email" placeholder="johndoe@email.com" />
+                <button className="footer-btn w-100" type="submit">Submit</button>
+            </div>
+        </form>
+        </Col>
+    <Col className="my-3 d-flex flex-column bg-success justify-content-between my-md-0">
     <h5 className="text-md-center mb-3 text-center">Verified Payment Methods</h5>
-    <div className="d-flex flex-row align-items-center justify-content-between">
+    <div className="d-flex flex-md-row align-items-center">
         {
             [mastercard, visa, verve].map((icon, index)=>{
                 return(
                     <img src={icon}
-                    alt="payment platforms" style={{width: "60px"}} className="mx-4" key={index} />
+                    alt="payment platforms"
+                    style={{width: "25%"}}  className="mx-auto" key={index} />
                 )
             })
         }
     </div>
 
     </Col>
-        <Col>
-        <form action="" className="my-3 my-md-0">
-            <h5 className="text-center mb-3">Get notified of new arrivals</h5>
-            <div>
-                <input className="footer__input mb-2 w-md-75 w-100" type="email" placeholder="johndoe@email.com" />
-                <button className="footer-btn w-md-75 w-100" type="submit">Submit</button>
-            </div>
-        </form>
-        </Col>
-   
     </Row>
     <div className="d-flex flex-column flex-md-row align-items-center justify-content-between" style={{marginTop: "5.5rem"}}>
     <div className="d-flex justify-content-around align-items-center">
